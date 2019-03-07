5361c5e919da504950c711220d7b00898ab70ab1
abc515952853 <123456789@qq.com>
2019-03-07 17:37:24 +0800

    222

---
+++ testcase/111.py
@@ -0,0 +1,38 @@
+import unittest
+import ddt
+from common import ReadExcl,ReadDB
+import ReadConfig 
+import requests
+import json 
+
+apiversion = 'api/AppVersion'
+apiread = 'api/AppVersion/{0}/Read'
+
+sheet_name = "AppVersion"
+
+excel = ReadExcl.Xlrd()
+
+
+@ddt.ddt
+class AppVersion(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.readdb = ReadDB.Pyodbc()
+        self.readconfig=ReadConfig.ReadConfig()
+
+        self.readdb.DBDelete("[AppVersionOpenRecord]")
+        self.readdb.DBDelete("[AppVersion]")
+
+    @classmethod
+    def tearDownClass(self):
+        self.readdb.DBClose()
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @ddt.data(*excel.get_xls_next(sheet_name))
+    def test_AppVersion(self,data):
+        isnewversion = str(data["isnewversion"])
